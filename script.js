e768ae8e051464c14b453001d73fbafc67f30553
ASPiRe Gray <afoninsergey@mail.ru>
2022-04-12 11:54:24 +0300

    Add for, while, if

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-(function() {
-  "use ctrict";
-
-  const numberOfFilms = +prompt('Сколько фильмов?', '');
-
+let numberOfFilms = prompt('Сколько фильмов?', '');
+  while (numberOfFilms < 0 || numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = prompt('Сколько фильмов?', '');
+  }
+  
   console.log(numberOfFilms);
+  
 
   const personalMuvieDB = {
     count: numberOfFilms,
@@ -12,15 +13,28 @@
     genres: [],
     privat: false
   };
+  
 
-  let a = prompt('Один из последних фильмов', ''),
-      b = +prompt('На сколько его оцените?', ''),
-      c = prompt('Один из последних фильмов', ''),
-      d = +prompt('На сколько его оцените?', '');
-
-  personalMuvieDB.movies[a] = b;
-  personalMuvieDB.movies[c] = d;
-
+  for (let i = 0; i < 2; i++ ) {
+    let a = prompt('Один из последних фильмов', ''),
+        b = +prompt('На сколько его оцените?', '');
+    if (a == "" || b == "" || a == null || b == null || a.length > 50 || b.length > 50) {
+      i--;
+    } else {
+      personalMuvieDB.movies[a] = b;
+    }        
+  }
   console.log(personalMuvieDB);
 
-})();
+  if (personalMuvieDB.count < 10) {
+    console.log("МАЛО!");
+  } else if (personalMuvieDB.count >= 10 && personalMuvieDB.count < 30) {
+    console.log("НОРМАЛЬНО!");
+  } else if (personalMuvieDB.count >= 30) {
+    console.log("СУПЕР!");
+  } else {
+    console.log("ОШИБКА!");
+  }
+  
+
+  
